d8e418f180078b0245d7fc0ef32fa053f5fe24dd
Luigi Calori <luigi.calori@gmail.com>
2017-06-01 15:52:26 +0200

    separate hosts in client and server

--- hosts/marconi/config/config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-spack repo add @{RCM_DEPLOY_ROOTPATH}/repo --scope site
-spack  compiler find --scope site
-#spack info rcm
-#spack install --verbose py-flake8
-#spack find
-#spack module  rm -y
-#spack module refresh -y
--- hosts/marconi/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- mkdir build_gnu
- source /marconi/home/userinternal/lcalori0/spack/RCM_spack_deploy/deploy/spack/share/spack/setup-env.sh
- module load cmake
- module load git
- module load libpthread-stubs
- module load autoload openmpi
-
